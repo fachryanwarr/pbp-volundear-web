33e5c88137c66d18504f961d4512822507a80737
mohamadarvin25 <mohamadarvin84@gmail.com>
2022-10-29 19:04:05 +0700

    nambahin form daftar relawan

--- volundear/urls.py
+++ volundear/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('daftarwilayah/', include('daftarwilayah.urls')),
     path('', include('daftarwilayah.urls')),
+    path('daftarrelawan/', include('daftarrelawan.urls')),
 ]
