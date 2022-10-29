cbcdf0ffe394bcf1a223023bf0d5c653c8a88f5a
Fachry Anwar <fachryanwar2926@gmail.com>
2022-10-29 20:49:35 +0700

    Update daftar wilayah

--- daftarwilayah/views.py
+++ daftarwilayah/views.py
@@ -17,3 +17,50 @@ def get_wilayah(request):
     wilayah_items = Wilayah.objects.all()
 
     return HttpResponse(serializers.serialize("json", wilayah_items), content_type="application/json")
+
+def add_wilayah(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        kota = request.POST.get('kota')
+        address = request.POST.get('address')
+        kuota_max = request.POST.get('kuota_max')
+        description = request.POST.get('description')
+        kebutuhan = request.POST.get('kebutuhan')
+        jangka_waktu = request.POST.get('jangka_waktu')
+
+        wilayah = Wilayah.objects.create(user=request.user, name=name, kota=kota, address=address,
+            kuota_max=kuota_max, description=description, kebutuhan=kebutuhan, jangka_waktu=jangka_waktu)
+        
+        hasil = {
+            'fields':{
+                'name':wilayah.name,
+                'kota':wilayah.kota,
+                'address':wilayah.address,
+                'kuota_max':wilayah.kuota_max,
+                'description':wilayah.description,
+                'kebutuhan':wilayah.kebutuhan,
+                'jangka_waktu':wilayah.jangka_waktu
+            },
+            'pk':wilayah.pk
+        }
+
+    return JsonResponse(hasil)
+
+def get_wilayah_detail(request, id):
+    wilayah = Wilayah.objects.get(pk = id)
+
+    hasil = {
+            'fields':{
+                'name':wilayah.name,
+                'kota':wilayah.kota,
+                'kebutuhan':wilayah.kebutuhan,
+                'address':wilayah.address,
+                'kuota_max':wilayah.kuota_max,
+                'kuota_terisi':wilayah.kuota_terisi,
+                'description':wilayah.description,
+                'jangka_waktu':wilayah.jangka_waktu,
+            },
+            'pk':wilayah.pk
+        }
+    return JsonResponse(hasil)
+
--- volundear/urls.py
+++ volundear/urls.py
@@ -5,5 +5,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('daftarwilayah/', include('daftarwilayah.urls')),
-    path('', include('daftarwilayah.urls')),
 ]
