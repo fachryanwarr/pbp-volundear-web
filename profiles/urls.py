c0dbc1bb8f1629e0c61c9ad887062c6d5dc727a5
Fachry Anwar <fachryanwar2926@gmail.com>
2022-11-02 20:58:55 +0700

    Add style to profile

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from profiles.views import show_profile, profile_relawan
+from profiles.views import show_profile, profile_relawan, show_edit, edit, get_detail
 
 app_name = "profiles"
 
 urlpatterns = [
     path("", show_profile, name='show_profile'),
-    path("profile_relawan", profile_relawan, name='profile_relawan'),
-    
+    path("show_edit/", show_edit, name="show_edit"),
+    path("profile_relawan/", profile_relawan, name='profile_relawan'),
+    path("edit/", edit, name="edit"),
+    path("get_detail/", get_detail, name='get_detail'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -5,16 +5,16 @@ from django.http.response import HttpResponse
 from authentication.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth
+from profiles.models import DetailedUserData
+from django.http import HttpResponse, JsonResponse
 
-
-# Create your views here.
 @login_required(login_url='/auth/login/')
 def show_profile(request):
     current_user = auth.get_user(request)
 
     account = DetailedUserData.objects.filter(user=request.user)
     if len(account) == 0:
-        return redirect('profiles:edit')
+        return redirect('profiles:show_edit')
     
     if current_user.is_relawan:
         return redirect('profiles:profile_relawan')
@@ -37,4 +37,45 @@ def profile_admin(request):
     account = DetailedUserData.objects.filter(user=request.user)
     response = {'account':account}
     return render(request, 'AdminProfile.html', response)
+
+@login_required(login_url='/auth/login/')
+def edit(request):
+    if request.method == "POST":
+        nama = request.POST.get('nama')
+        tanggal_lahir = request.POST.get('tanggal_lahir')
+        gender = request.POST.get('gender')
+        telp = request.POST.get('telp')
+        alamat = request.POST.get('alamat')
+        pekerjaan = request.POST.get('pekerjaan')
+
+        detailUser = DetailedUserData(user=request.user, name=nama, gender=gender, phone_number=telp, pekerjaan=pekerjaan, alamat=alamat, tanggal_lahir=tanggal_lahir)
+        detailUser.save()
+        
+        return redirect('profiles:show_profile')
+
+@login_required(login_url='/auth/login/')
+def show_edit(request):
+    content = {}
+    
+    return render(request, "edit.html", content)
+
+def get_detail(request):
+    detail = DetailedUserData.objects.filter(user=request.user)
+
+    hasil = {
+            'fields':{
+                'nama':detail[0].name,
+                'gender':detail[0].gender,
+                'phone_number':detail[0].phone_number,
+                'pekerjaan':detail[0].pekerjaan,
+                'alamat':detail[0].alamat,
+                'tanggal_lahir':detail[0].tanggal_lahir,
+            },
+            'pk':detail[0].pk
+        }
+
+    return JsonResponse(hasil)
+
+
+
 
