d6d07961deeb23c0115269465329c5a278ad085d
Fachry Anwar <fachryanwar2926@gmail.com>
2022-11-02 19:31:18 +0700

    initial profile

--- profiles/models.py
+++ profiles/models.py
@@ -6,3 +6,6 @@ class DetailedUserData(models.Model):
     name = models.CharField(max_length=50)
     gender = models.CharField(max_length=10)
     phone_number = models.IntegerField()
+    pekerjaan = models.CharField(max_length=30)
+    alamat = models.TextField()
+    tanggal_lahir = models.CharField(max_length=30)
--- profiles/views.py
+++ profiles/views.py
@@ -3,7 +3,6 @@ from .models import DetailedUserData
 from django.http.response import HttpResponseRedirect
 from django.http.response import HttpResponse
 from authentication.models import User
-from profiles.forms import UpdateProfil
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth
 
@@ -27,37 +26,15 @@ def show_profile(request):
 def profile_relawan(request):
     account = DetailedUserData.objects.filter(user=request.user)
     response = {'account':account}
-    return render(request, 'RelawanProfile.html', response)
+    return render(request, 'profile_relawan.html', response)
 
 def profile_PJ(request):
     account = DetailedUserData.objects.filter(user=request.user)
     response = {'account':account}
-    return render(request, 'PJProfile.html', response)
+    return render(request, 'profile_PJ.html', response)
 
 def profile_admin(request):
     account = DetailedUserData.objects.filter(user=request.user)
     response = {'account':account}
     return render(request, 'AdminProfile.html', response)
 
-@login_required(login_url='/auth/login/')
-def edit(request, *args, **kwargs):
-    context = {}
-    if request.POST:
-        form = UpdateProfil(request.POST, instance=request.user)
-
-        if form.is_valid():
-            form.save()
-
-            return redirect('profiles:show_profile')
-        else :
-            form = UpdateProfil(request.POST, instance= request.user,       
-                    initial = {
-                        "nama" : account.nama,
-                        "jenis_kelamin" : account.jenis_kelamin,
-                        "institusi" : account.institusi,
-                        "email" : account.email,
-                        "kontak" : account.kontak
-                    }   
-                ) 
-            context['form'] = form
-            return render(request, 'FormProfil.html', context)
