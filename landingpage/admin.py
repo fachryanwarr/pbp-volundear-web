6802654995d801d0ea366ae0f053f96c0fe96974
Fachry Anwar <fachryanwar2926@gmail.com>
2022-11-02 21:58:57 +0700

    fix deployment

--- landingpage/admin.py
+++ landingpage/admin.py
@@ -1,8 +1,5 @@
-from tkinter.messagebox import QUESTION
 from django.contrib import admin
 from landingpage.models import Faq, Feedback
 
-
 admin.site.register(Faq)
 admin.site.register(Feedback)
-# Register your models here.
