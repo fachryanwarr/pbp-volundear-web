203a15932926df77a355941e31467abf902b14cd
Fachry Anwar <fachryanwar2926@gmail.com>
2022-12-03 14:37:39 +0700

    Fix model fields

--- profiles/models.py
+++ profiles/models.py
@@ -5,7 +5,7 @@ class DetailedUserData(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     gender = models.CharField(max_length=10, default="-")
-    phone_number = models.IntegerField(default="")
+    phone_number = models.CharField(max_length=15, default="")
     pekerjaan = models.CharField(max_length=30, default="")
     alamat = models.TextField(default="")
     tanggal_lahir = models.CharField(max_length=30, default="-")
